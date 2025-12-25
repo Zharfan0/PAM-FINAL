f27c7386d46ececcf9b21ce0eec9f4ba52d50f98
Zharfan <zharfanwidyadana@gmail.com>
2025-12-25 22:14:43 +0700

    membuat dapat bernavigasi ke CreateSuratActivity.kt

--- app/src/main/java/com/example/finalprojectpam/Persuratan/PersuratanActivity.kt
+++ app/src/main/java/com/example/finalprojectpam/Persuratan/PersuratanActivity.kt
@@ -2,6 +2,7 @@ package com.example.finalprojectpam.Persuratan
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.navigation.NavigationView
@@ -10,6 +11,7 @@ import androidx.appcompat.widget.Toolbar
 import com.example.finalprojectpam.LandingActivity
 import com.example.finalprojectpam.R
 import com.example.finalprojectpam.SignInActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class PersuratanActivity : AppCompatActivity() {
 
@@ -17,13 +19,25 @@ class PersuratanActivity : AppCompatActivity() {
     private lateinit var navigationView: NavigationView
     private lateinit var toolbar: Toolbar
 
+    private lateinit var btnBuatSurat: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_persuratan)
+        btnBuatSurat = findViewById(R.id.btnBuatSurat)
+
 
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationView)
         toolbar = findViewById(R.id.toolbar)
+
+        //btnNavigasi membuat surat
+        btnBuatSurat.setOnClickListener {
+            val intent = Intent(this, CreateSuratActivity::class.java)
+            startActivity(intent)
+        }
+
 
         // Set toolbar sebagai ActionBar
         setSupportActionBar(toolbar)
@@ -38,6 +52,8 @@ class PersuratanActivity : AppCompatActivity() {
         )
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+
+
 
         // Navigasi menu
         navigationView.setNavigationItemSelectedListener { menuItem ->
@@ -56,8 +72,13 @@ class PersuratanActivity : AppCompatActivity() {
                 }
 
                 R.id.menu_logout -> {
-                    startActivity(Intent(this, SignInActivity::class.java))
+                    FirebaseAuth.getInstance().signOut()
+
+                    val intent = Intent(this, SignInActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
                     finish()
+
                 }
             }
             drawerLayout.closeDrawers()
