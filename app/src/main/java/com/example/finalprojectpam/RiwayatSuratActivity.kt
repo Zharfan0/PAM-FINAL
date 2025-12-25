5456b9ed771876b37e9e68d547000c9bae504ea0
Zharfan <zharfanwidyadana@gmail.com>
2025-12-25 21:15:28 +0700

    AndroidManifest.xml: mendaftarkan RiwayatSuratActivity.kt
    RiwayatSuratActivity.kt: update

--- app/src/main/java/com/example/finalprojectpam/RiwayatSuratActivity.kt
+++ app/src/main/java/com/example/finalprojectpam/RiwayatSuratActivity.kt
@@ -1,4 +1,64 @@
 package com.example.finalprojectpam
 
-class RiwayatSuratActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.navigation.NavigationView
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.widget.Toolbar
+import com.example.finalprojectpam.R
+
+class RiwayatSuratActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_riwayat_surat)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        toolbar = findViewById(R.id.toolbar)
+
+        // Set toolbar sebagai ActionBar
+        setSupportActionBar(toolbar)
+
+        // Tombol hamburger (≡)
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open,
+            R.string.close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Navigasi menu
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+
+                R.id.menu_home -> {
+                    startActivity(Intent(this, LandingActivity::class.java))
+                }
+
+                R.id.menu_daftar_surat -> {
+                    startActivity(Intent(this, PersuratanActivity::class.java))
+                }
+
+                R.id.menu_riwayat -> {
+                }
+
+                R.id.menu_logout -> {
+                    startActivity(Intent(this, SignInActivity::class.java))
+                    finish()
+                }
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
+    }
 }
