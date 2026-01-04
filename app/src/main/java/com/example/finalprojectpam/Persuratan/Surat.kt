80257c88bdf13eda99c0a88cab749a275fbd8dcd
Zharfan <zharfanwidyadana@gmail.com>
2026-01-04 19:54:14 +0700

    membuat dataclass utk menyimpan obj

---
+++ app/src/main/java/com/example/finalprojectpam/Persuratan/Surat.kt
@@ -0,0 +1,10 @@
+package com.example.finalprojectpam.Persuratan
+
+data class Surat(
+    val tanggal: String = "",
+    val perihal: String = "",
+    val isi: String = "",
+    val ketua: String = "",
+    val tujuan: String = "",
+    val nomor: String = ""
+)
