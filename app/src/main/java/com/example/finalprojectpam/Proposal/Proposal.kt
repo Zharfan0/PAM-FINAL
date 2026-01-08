510287ad5cd359773d1c709b9d22dd79778356c9
Zharfan <zharfanwidyadana@gmail.com>
2026-01-08 13:09:53 +0700

    membuat data class proposal untuk menyimpan data

---
+++ app/src/main/java/com/example/finalprojectpam/Proposal/Proposal.kt
@@ -0,0 +1,13 @@
+package com.example.finalprojectpam.Proposal
+
+import android.database.sqlite.SQLiteProgram
+
+data class Proposal(
+    var id: String = "",
+    val NmProgram: String = "",
+    val TglProgram: String = "",
+    val KetuPlak: String = "",
+    val TtlDana: String = "",
+    val Divisi: String = "",
+    val Status: String = "Draft"
+)
